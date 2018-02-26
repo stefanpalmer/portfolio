81bbf56f30de17bea22c5a870ca21051dc4c354e
stefanpalmer <palmerstefan@yahoo.com>
2018-02-26 10:25:35 +0100

    Add scroll effect and fix links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function(){
   new WOW().init();
 });
+
+$(function(){
+  $("a.links").click(function(event){
+    event.preventDefault();
+    var section = $(this).attr("href");
+    $('html, body').animate({
+      scrollTop: $(section).offset().top
+    }, 1250);
+  });
+})
